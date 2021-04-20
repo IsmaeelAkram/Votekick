37f39148aa1c4892d0f46e200d71640aaf73a3a7
Ismaeel <ismaeelakram01@gmail.com>
2021-04-20 14:39:55 -0400

    Add kick function

---
+++ utils/kick.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from tinydb import TinyDB, where
+from utils import embed
+
+
+async def kick(bot: commands.Bot, vote_message: discord.Message, db: TinyDB):
+    request_id = (
+        vote_message.embeds[0].footer.text.replace("Request ID: ", "").split("|")[0]
+    )
+    request = db.table("requests").search(where("request_id") == request_id)[0]
+    user = await bot.fetch_user(request["user_id"])
+
+    await vote_message.guild.kick(user, request["reason"])
+    await vote_message.channel.send(
+        embed=embed.Embed(
+            description=f"Kicked {user.mention} for `{request['reason']}.`",
+            footer="Request ID: " + request_id,
+        )
+    )
